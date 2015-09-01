597ca2ecfb4880f463f25aba6fd733325d7924de
GuusHamm <guushamm@gmail.com>
2015-09-01 15:03:40 +0200

    Fixed CI

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Guus on 2015-09-01.
+ */
+public class MainClass
+{
+    public static void main(String[] args)
+    {
+        System.out.println("test");
+    }
+}
---
+++ src/test/MainClassTEst.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Guus on 2015-09-01.
+ */
+public class MainClassTEst
+{
+    @Test
+    public void test1(){
+        assertEquals(1,1);
+    }
+}
