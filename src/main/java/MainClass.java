856e0b2c3d0bda238ce5ef2d0ccff3d7f09702d7
GuusHamm <Dihw8vGH>
2015-09-28 13:44:56 +0200

    Added generic tests

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,13 +1,13 @@
 /**
  * Created by Guus on 2015-09-01.
  */
-import javax.microedition.lcdui.game.Sprite;
 import java.awt.*;
 
 public class MainClass
 {
     public static void main(String[] args)
     {
+
         System.out.println("test");
     }
 }
