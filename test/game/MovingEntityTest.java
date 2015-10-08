99244610ec8259de7537941a093daf29e239be81
Wouter Vanmulken <woutervanmulken@msn.com>
2015-10-08 14:26:39 +0200

    syncing

--- test/game/MovingEntityTest.java
+++ test/game/MovingEntityTest.java
@@ -1,14 +1,21 @@
 package game;
 
+import game.role.Tank;
 import junit.framework.TestCase;
+
+import javafx.geometry.Point2D;
+
 
 /**
  * Created by Wouter Vanmulken on 8-10-2015.
  */
 public class MovingEntityTest extends TestCase {
 
+    Bullet bullet;
+
     public void setUp() throws Exception {
         super.setUp();
+        bullet= new Bullet(new HumanCharacter(new Game(1,1,false,100), new Point2D(1, 1), "testplayer", new Tank() ,new Point2D(1,1)) ,new Point2D(1,1));
 
     }
 
