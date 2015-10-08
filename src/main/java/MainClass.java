7917137147d6a173b0c0fe0fb20c1b00f55a3ce5
Teun <mj.willems@student.fontys.nl>
2015-10-08 14:04:27 +0200

    Added LibGDX, might take some downloading

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,13 +1,18 @@
-/**
- * Created by Guus on 2015-09-01.
- */
-import java.awt.*;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class MainClass
 {
+    public static final String NAME = "Call of Cactus: Desert Warfare";
+
     public static void main(String[] args)
     {
+        System.out.println("Starting Call Of Cactus...");
 
-        System.out.println("test");
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = MainClass.NAME;
+        config.width = 800;
+        config.height = 480;
+        new LwjglApplication(new CallOfCactusGame(), config);
     }
 }
