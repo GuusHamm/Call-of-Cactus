17ef729b048204f0a1c7c9dc88904c3a89a63c78
GuusHamm <guushamm@gmail.com>
2015-10-12 10:57:32 +0200

    You get a test and you get a test and you

---
+++ test/account/AccountTest.java
@@ -0,0 +1,37 @@
+package account;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by xubuntu on 12-10-15.
+ */
+public class AccountTest
+{
+    
+    @Before
+    public void setUp() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testJoinGame() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testExitGame() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testVerifyAccount() throws Exception
+    {
+
+    }
+}
---
+++ test/game/BulletTest.java
@@ -0,0 +1,30 @@
+package game;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by xubuntu on 12-10-15.
+ */
+public class BulletTest
+{
+    
+    @Test
+    public void testGetVelocity() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testGetDamage() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testGetShooter() throws Exception
+    {
+
+    }
+}
