753255309cf472f097a3f41674945828b86cda50
Jim Sanders <j.sanders@student.fontys.nl>
2015-10-17 22:38:27 +0200

    Added HUD initialization to GameScreen. Added GameScreenTest without code.

---
+++ test/game/GameScreenTest.java
@@ -0,0 +1,26 @@
+package game;
+
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Jim on 17-10-2015.
+ */
+public class GameScreenTest extends TestCase {
+
+    @Before
+    public void setUp() throws Exception
+    {
+        //Todo Implent Test
+
+    }
+
+    @Test
+    public void testDrawHud() throws Exception
+    {
+        //Todo Implent Test
+
+    }
+}
+
