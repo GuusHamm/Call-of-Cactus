d2d169df9a5528c56e1348aaf7244b9be04fb1bb
binq1000 <t.daniels@student.fontys.nl>
2015-11-02 20:01:07 +0100

    Made Test that cover everything for Account

--- test/account/AccountTest.java
+++ test/account/AccountTest.java
@@ -1,34 +1,59 @@
 package account;
 
+import game.Game;
 import org.junit.Before;
 import org.junit.Test;
+import testClasses.GameMockup;
 
 /**
  * Created by xubuntu on 12-10-15.
  */
 public class AccountTest {
 
+	Account account;
+    Game game;
+
 	@Before
 	public void setUp() throws Exception {
 		//Todo Implent Test
+
+        // Account(String username, String firstName, String lastName)
+        account = new Account("Testuser", "Test", "User");
+        game = new GameMockup();
 
 	}
 
 	@Test
 	public void testJoinGame() throws Exception {
 		//Todo Implent Test
-
+        try {
+            account.joinGame(game);
+        }
+        catch (UnsupportedOperationException use) {
+            //Not yet implemented
+        }
 	}
 
 	@Test
 	public void testExitGame() throws Exception {
 		//Todo Implent Test
+        try {
+            account.exitGame();
+        }
+        catch (UnsupportedOperationException use) {
+            //Not yet implemented
+        }
 
 	}
 
 	@Test
 	public void testVerifyAccount() throws Exception {
 		//Todo Implent Test
-
+        try {
+            account.verifyAccount("Testuser", "EncryptedPasswod");
+        }
+        catch (UnsupportedOperationException use) {
+            //Not yet implemented
+        }
 	}
 }
