80ef2d32ac136d1819c1bc675ce17c0b3a5a8d92
Wouter Vanmulken <woutervanmulken@msn.com>
2015-11-10 22:52:17 +0100

    what i have so far and it doesnt work i have no idea why but ill fix it

--- src/main/java/Multiplayer/Client.java
+++ src/main/java/Multiplayer/Client.java
@@ -1,11 +1,12 @@
 package Multiplayer;
 
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Net;
 import com.badlogic.gdx.net.Socket;
 import com.badlogic.gdx.net.SocketHints;
 
-import java.io.PrintStream;
+import java.io.IOException;
 
 /**
  * Created by woute on 9-11-2015.
@@ -23,26 +24,24 @@ public class Client {
         socketHints.keepAlive=true;
 
         //create the socket and connect to the server entered in the text box ( x.x.x.x format ) on port 9021
-        Socket socket = Gdx.net.newClientSocket(Net.Protocol.TCP, "127.0.0.1", 9021, socketHints);
+//        Socket socket = Gdx.net.newClientSocket(Net.Protocol.TCP, "127.0.0.1", 9021, socketHints);
 
     }
     public void  sendMessage(String text){
-
+        text = "kkkkkkkkkkk";
+        // When the button is clicked, get the message text or create a default string value
         String textToSend = new String();
-        if(!socket.isConnected()){
-            socket = Gdx.net.newClientSocket(Net.Protocol.TCP, "127.0.0.1", 9021, socketHints);
-        }
-        textToSend = text + "\n";
+            textToSend = text + ("\n"); // Brute for a newline so readline gets a line
 
         SocketHints socketHints = new SocketHints();
-
+        // Socket will time our in 4 seconds
+        socketHints.connectTimeout = 4000;
+        //create the socket and connect to the server entered in the text box ( x.x.x.x format ) on port 9021
+        Socket socket = Gdx.net.newClientSocket(Net.Protocol.TCP, "127.0.0.1", 9021, socketHints);
         try {
             // write our entered message to the stream
-            //socket.getOutputStream().write(textToSend.getBytes());
-
-            PrintStream ps = new PrintStream(socket.getOutputStream());
-            ps.println(textToSend);
-        } catch (Exception e) {
+            socket.getOutputStream().write(textToSend.getBytes());
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
--- src/main/java/Multiplayer/Server.java
+++ src/main/java/Multiplayer/Server.java
@@ -17,84 +17,37 @@ public class Server {
 
     public Server() {
 
-        new Thread(new Runnable() {
+                new Thread(new Runnable() {
 
-        @Override
-        public void run() {
-    //        ServerSocketHints serverSocketHint = new ServerSocketHints();
-            // 0 means no timeout.  Probably not the greatest idea in production!
-  //          serverSocketHint.acceptTimeout = 100;
+                    @Override
+                    public void run() {
+                        ServerSocketHints serverSocketHint = new ServerSocketHints();
+                        // 0 means no timeout.  Probably not the greatest idea in production!
+                        serverSocketHint.acceptTimeout = 0;
 
-            // Create the socket server using TCP protocol and listening on 9021
-            // Only one app can listen to a port at a time, keep in mind many ports are reserved
-            // especially in the lower numbers ( like 21, 80, etc )
-//            ServerSocket serverSocket = Gdx.net.newServerSocket(Net.Protocol.TCP, 9021, serverSocketHint);
+                        // Create the socket server using TCP protocol and listening on 9021
+                        // Only one app can listen to a port at a time, keep in mind many ports are reserved
+                        // especially in the lower numbers ( like 21, 80, etc )
+                        ServerSocket serverSocket = Gdx.net.newServerSocket(Net.Protocol.TCP, 9021, serverSocketHint);
 
-            // Loop forever
-//            while (true) {
-//                // Create a socket
-//                Socket socket = serverSocket.accept();
-//
-//                // Read data from the socket into a BufferedReader
-//                BufferedReader buffer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//
-//                try {
-//                    // Read to the next newline (\n) and display that text on labelMessage
-//                    System.out.println(buffer.readLine());
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-            //Socket sock = serverSocket.accept(new SocketHints());
+                        // Loop forever
+                        while (true) {
+                            // Create a socket
+                            Socket socket = serverSocket.accept(null);
 
-//            while(true)
-//            {
-//                //Reading the message from the client
-//                InputStream is = sock.getInputStream();
-//                InputStreamReader isr = new InputStreamReader(is);
-//                BufferedReader br = new BufferedReader(isr);
-//                String number = "aaaa";
-//                try {
-//                    System.out.println("f");
-//                    System.out.println(br.readLine());
-//                    System.out.println("k");
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//                System.out.println("Message received from client is "+number);
-//
-//            }
+                            // Read data from the socket into a BufferedReader
+                            BufferedReader buffer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-            ServerSocketHints serverSocketHint = new ServerSocketHints();
-            // 0 means no timeout.  Probably not the greatest idea in production!
-            serverSocketHint.acceptTimeout = 10000;
-
-            // Create the socket server using TCP protocol and listening on 9021
-            // Only one app can listen to a port at a time, keep in mind many ports are reserved
-            // especially in the lower numbers ( like 21, 80, etc )
-            ServerSocket serverSocket = Gdx.net.newServerSocket(Net.Protocol.TCP, 9021, serverSocketHint);
-            Socket socket = serverSocket.accept(null);
-
-            // Loop forever
-            while(true){
-                // Create a socket
-
-                // Read data from the socket into a BufferedReader
-                BufferedReader buffer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-                try {
-                    // Read to the next newline (\n) and display that text on labelMessage
-                    System.out.println(buffer.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                            try {
+                                // Read to the next newline (\n) and display that text on labelMessage
+                                System.out.println(buffer.readLine());
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }).start(); // And, start the thread running
             }
         }
-    }).start(); // And, start the thread running
-}
 
-    public static void main(String args[])
-    {
-        Server s = new Server();
-    }
-}
+//http://www.gamefromscratch.com/post/2014/03/11/LibGDX-Tutorial-10-Basic-networking.aspx
