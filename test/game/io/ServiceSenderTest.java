b750d7c2d8273e8aa4aff90d9ac1e80505841e80
Teun <teun.willems@live.nl>
2015-11-17 10:50:00 +0100

    Added new Unittest

---
+++ test/game/io/ServiceSenderTest.java
@@ -0,0 +1,26 @@
+package game.io;
+
+import account.Account;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Teun on 17-11-2015.
+ */
+public class ServiceSenderTest {
+
+    private ServiceSender serviceSender;
+
+    @Before
+    public void setUp() throws Exception {
+        serviceSender = new ServiceSender();
+    }
+
+    @Test
+    public void testCreateAccount() throws Exception {
+        Account a = new Account("TEUN");
+        assertFalse("Account shouldn't be created", serviceSender.createAccount(a));
+    }
+}
