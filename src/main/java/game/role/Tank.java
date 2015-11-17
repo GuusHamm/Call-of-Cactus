8a5304637d7c0e85d9c33dbf4cb61b95103b0007
guushamm <guushamm@gmail.com>
2015-11-17 13:30:06 +0100

    Code Review

---
+++ src/main/java/game/role/Tank.java
@@ -0,0 +1,15 @@
+package game.role;
+
+public class Tank extends Role {
+
+	/**
+	 * Makes a new instance of the class Tank
+	 */
+	public Tank() {
+		// TODO - implement Tank.Tank
+		throw new UnsupportedOperationException();
+	}
+
+	// TODO - implements unique elements for this role
+
+}
