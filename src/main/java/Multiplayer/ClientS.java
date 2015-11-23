e1f63acd57681903c6b9579f7bf26e8dda258fe3
Wouter Vanmulken <woutervanmulken@msn.com>
2015-11-23 11:59:48 +0100

    Socket implementation

--- src/main/java/Multiplayer/ClientS.java
+++ src/main/java/Multiplayer/ClientS.java
@@ -1,62 +1,45 @@
-package Multiplayer;
+package multiplayer;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Net;
-import com.badlogic.gdx.net.Socket;
-import com.badlogic.gdx.net.SocketHints;
-
-import java.io.PrintStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 /**
- * Created by woute on 19-11-2015.
+ * Created by woute on 23-11-2015.
  */
 public class ClientS {
 
     Socket socket;
-	SocketHints socketHints;
+    PrintWriter out;
+    BufferedReader in;
 
-	public ClientS() {
+    public ClientS(){
 
-		socketHints = new SocketHints();
-		// Socket will time our in 4 seconds
-		socketHints.connectTimeout = 4000;
-		socketHints.keepAlive = true;
+        try {
+            socket = new Socket("127.0.0.1", 8008);
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
 
-		//create the socket and connect to the server entered in the text box ( x.x.x.x format ) on port 8008
-		socket = Gdx.net.newClientSocket(Net.Protocol.TCP, "127.0.0.1", 8008, socketHints);
-		for (int i = 0; i < 10; i++) {
-			sendMessage("kkk");
-		}
-	}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-	public static void main(String args[]) {
+    }
+    public void sendMessage(String message){
 
+        try {
+            out.println(message);
+        }catch (Exception e){e.printStackTrace();}
 
+    }
 
-	}
-
-	public void sendMessage(String text) {
-
-		String textToSend = new String();
-		if (!socket.isConnected()) {
-			socket = Gdx.net.newClientSocket(Net.Protocol.TCP, "127.0.0.1", 8008, socketHints);
-		}
-		textToSend = "playrandombulletsound";
-
-		SocketHints socketHints = new SocketHints();
-
-		System.out.println("tried sending message");
-		try {
-			// write our entered message to the stream
-			//socket.getOutputStream().write(textToSend.getBytes());
-
-			PrintStream ps = new PrintStream(socket.getOutputStream());
-			ps.println(textToSend);
-			System.out.println("message Sent");
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args)
+    {
+        ClientS cs2 = new ClientS();
+        cs2.sendMessage("kkkk");
+    }
 
 }
