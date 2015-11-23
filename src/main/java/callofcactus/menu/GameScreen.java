0d6627e795da855c6d66f142204431e329718a3b
Teun <teun.willems@live.nl>
2015-11-23 11:27:05 +0100

    Addded Initilizer
    # Conflicts:
    #	src/main/java/callofcactus/GameInitializer.java

--- src/main/java/callofcactus/menu/GameScreen.java
+++ src/main/java/callofcactus/menu/GameScreen.java
@@ -60,6 +60,7 @@ public class GameScreen implements Screen {
 	private SpriteBatch backgroundBatch;
 	private BackgroundRenderer backgroundRenderer;
 	private SpriteBatch AIBatch;
+	private SpriteBatch batch;
 	//  MAP variables
 	private CallOfCactusMap defaultMap;
 	private SpriteBatch mapBatch;
@@ -241,12 +242,12 @@ public class GameScreen implements Screen {
 	@Override
 	public void render(float v) {
 		//Check whether W,A,S or D are pressed or not
+		batch = gameInitializer.getBatch();
 		procesMovementInput();
 		game.compareHit();
 
 		game.getMovingEntities().stream().filter(e -> e instanceof HumanCharacter && ((HumanCharacter) e).getHealth() <= 0).forEach(e -> goToEndScreen());
 
-		SpriteBatch batch = gameInitializer.getBatch();
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
