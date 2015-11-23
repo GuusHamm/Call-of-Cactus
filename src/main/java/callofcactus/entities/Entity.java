86f250f92f0ad8ea2acaa77ff5d914be94ba33c8
Teun <teun.willems@live.nl>
2015-11-23 11:45:47 +0100

    Removed invalid code

--- src/main/java/callofcactus/entities/Entity.java
+++ src/main/java/callofcactus/entities/Entity.java
@@ -8,7 +8,7 @@ import com.sun.xml.internal.ws.developer.Serialization;
 
 import java.lang.annotation.Annotation;
 
-public abstract class Entity implements Serialization {
+public abstract class Entity {
 
 	public static int nxtID = 0;
 	protected int ID;
@@ -130,14 +130,5 @@ public abstract class Entity implements Serialization {
 
 		}
 		return health;
-	}
-	@Override
-	public String encoding() {
-		return null;
-	}
-
-	@Override
-	public Class<? extends Annotation> annotationType() {
-		return null;
 	}
 }
