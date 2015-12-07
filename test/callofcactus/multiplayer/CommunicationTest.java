eff22864b613a1d3c1c2cb93be20b27e59206097
Wouter Vanmulken <woutervanmulken@msn.com>
2015-12-07 13:17:17 +0100

    test

--- test/callofcactus/multiplayer/CommunicationTest.java
+++ test/callofcactus/multiplayer/CommunicationTest.java
@@ -1,6 +1,8 @@
 package callofcactus.multiplayer;
 
+import callofcactus.Administration;
 import callofcactus.MultiPlayerGame;
+import junit.framework.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -12,17 +14,34 @@ public class CommunicationTest {
     ServerS serverS;
     ClientS clientS;
     ClientSideServer clientSideServer;
+    MultiPlayerGame multiPlayerGame;
+    Administration administration;
 
     @BeforeClass
-    public void setUp() throws Exception {
+    public void setUp(){
+
         serverS = new ServerS(new MultiPlayerGame());
         clientS = new ClientS();
         clientSideServer = new ClientSideServer();
+        multiPlayerGame = new MultiPlayerGame();
+        multiPlayerGame.addSinglePlayerHumanCharacter();
+        administration = Administration.getInstance();
     }
 
     @Test
     public void testSendCommand() {
-        new Command(Command.methods.GET,null);
+
+
+        clientS.sendMessageAndReturn(new Command(Command.methods.GET,null,"entities"));
+        Assert.assertEquals(administration.getAllEntities(), multiPlayerGame.getAllEntities() );
+
+
+
+//        clientS.sendMessageAndReturn(new Command(Command.methods.POST,new Object[]{
+//                new Bullet(multiPlayerGame,new Vector2(10,10),multiPlayerGame.getPlayer(),1,1, GameTexture.texturesEnum.bulletTexture,10,10,10)
+//        }));
+//        clientS.sendMessageAndReturn(new Command(Command.methods.CHANGE,));
+
     }
 
 
