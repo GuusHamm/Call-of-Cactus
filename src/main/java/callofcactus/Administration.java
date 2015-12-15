d6418a899ad2773db0d10eec872d05012eacc317
GuusHamm <guushamm@gmail.com>
2015-12-15 11:49:29 +0100

    this is not what i want but shit is broken as fuck

--- src/main/java/callofcactus/Administration.java
+++ src/main/java/callofcactus/Administration.java
@@ -156,7 +156,7 @@ public class Administration {
             }
         }
 
-        return null;
+        return localPlayer;
     }
 
     public void setLocalPlayer(HumanCharacter localPlayer) {
