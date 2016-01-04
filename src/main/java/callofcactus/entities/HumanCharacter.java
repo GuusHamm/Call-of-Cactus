dc480fc2e89bc55faca96bddee97a39cdb4e8c8e
binq1000 <t.daniels@student.fontys.nl>
2016-01-04 10:17:26 +0100

    Setting Kills and deaths for test

--- src/main/java/callofcactus/entities/HumanCharacter.java
+++ src/main/java/callofcactus/entities/HumanCharacter.java
@@ -36,8 +36,8 @@ public class HumanCharacter extends Player implements Comparable {
     public HumanCharacter(IGame game, Vector2 location, String name, Role role, GameTexture.texturesEnum spriteTexture, int spriteWidth, int spriteHeight, boolean fromServer, Account account) {
         super(game, location, name, role, spriteTexture, spriteWidth, spriteHeight, fromServer, account);
         score = 0;
-        killCount = 0;
-        deathCount = 0;
+        killCount = account.getKillCount();
+        deathCount = account.getDeathCount();
     }
 
     /**
