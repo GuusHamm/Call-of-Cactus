f40a10990db139bc6041f70685dbd77c1fcd4c08
binq1000 <t.daniels@student.fontys.nl>
2016-01-04 14:27:03 +0100

    Testing

--- src/main/java/callofcactus/Administration.java
+++ src/main/java/callofcactus/Administration.java
@@ -269,7 +269,7 @@ public class Administration {
         this.scoreBoard.clear();
 
         for (HumanCharacter h : this.players) {
-            scoreBoard.put(h.getName(), h.getKillCount());
+            scoreBoard.put(h.getName(), h.getAccount().getKillCount());
         }
 
         return this.scoreBoard;
@@ -447,10 +447,8 @@ public class Administration {
     public void addSinglePlayerHumanCharacter() {
         HumanCharacter p;
         if (getLocalAccount() != null) {
-
             p = new HumanCharacter(null, new Vector2(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2), localAccount.getUsername(), new Soldier(), GameTexture.texturesEnum.playerTexture, 64, 26, false, getLocalAccount());
             //((HumanCharacter) p).setKillCount();
-
         }
         else {
             p = new HumanCharacter(null, new Vector2(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2), localAccount.getUsername(), new Soldier(), GameTexture.texturesEnum.playerTexture, 64, 26, false);
