cfc6796653593f0c84e91ae9058b92b69d09d5ed
woutie012006 <woutervanmulken@msn.com>
2016-01-05 11:53:45 +0100

    here is what i did so far

--- src/main/java/callofcactus/menu/MultiPlayerGameScreen.java
+++ src/main/java/callofcactus/menu/MultiPlayerGameScreen.java
@@ -35,6 +35,7 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.TimeUtils;
@@ -498,22 +499,19 @@ public class MultiPlayerGameScreen implements Screen {
                     playerSprite.setSize(width, height);
                     playerSprite.setOriginCenter();
 
-                    int screenX = (int) (p.getLocation().x - (camera.viewportWidth / 2));
-                    int screenY = (int) (p.getLocation().y - (camera.viewportHeight / 2));
+
+                    int screenX = (int) (p.getLocation().x );
+                    int screenY = (int) (p.getLocation().y);
 
                     int angle = 0;
                         if(p.getID() == administration.getLocalPlayer().getID()) {
-                            float mouseX = administration.getMouse().x;
-                            float mouseY = administration.getMouse().y;
-                            if (screenX > 0) {
-                                mouseX += screenX;
-                            }
-                            if (screenY > 0) {
-                                mouseY += screenY;
-                            }
+                            float mouseX = properties.get("height", Integer.class)-(administration.getMouse().x+ camera.position.x); //+ camera.viewportWidth;
+                            float mouseY = ((administration.getMouse().y)+ camera.position.y); //+ camera.viewportHeight;
                             Vector2 newMousePosition = new Vector2(mouseX, mouseY);
-                            angle = administration.angle(new Vector2(p.getLocation().x, (p.getLocation().y)), newMousePosition);
 
+                            angle = administration.angle(p.getLocation(), newMousePosition);
+
+                            System.out.println("fuck"+newMousePosition.toString());
                             administration.getLocalPlayer().setAngle(angle, true);
                         }
 //                        else{
